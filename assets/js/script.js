af0b64bdf5c7415ade4bee90903c9f342a2ee061
DrSYakovlev <s.o.yakovlev@gmail.com>
2023-12-14 18:23:42 +0000

    Update html frond, add event listeners, add layout of functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-console.log('connected');
+//get random reply
+let reply = ['rock', 'paper','scissors','well'];
+let gameType;
+let currentReply;
+
+document.getElementById('rock').addEventListener("click", myRock);
+document.getElementById('paper').addEventListener('click', myPaper);
+document.getElementById('scissors').addEventListener('click', myScissors);
+document.getElementById('well').addEventListener('click', myWell);
+
+function randomReply() {
+    console.log(reply[(Math.floor(Math.random() * reply.length))]);
+}
+
+function selection() {
+    console.log(document.getElementById('best-of-three').checked);
+    
+}
+
+function myRock() {
+    console.log('You selected Rock');
+    console.log(randomReply());
+
+}
+
+function myPaper() {
+    console.log('You selected Paper');
+}
+
+function myScissors() {
+    console.log('You selected Scissors');
+}
+
+function myWell() {
+    console.log('You selected Well');
+}
+
+gameType = selection();
+console.log(gameType);
+
+if (gameType) {
+    console.log('You are playing best-of-three game');
+} else {
+    console.log('You are playing a single-shot game');
+}
