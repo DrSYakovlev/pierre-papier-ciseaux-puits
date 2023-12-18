233f7971097749fd0420ef99f56985c259a4f9fa
DrSYakovlev <s.o.yakovlev@gmail.com>
2023-12-18 18:15:13 +0000

    Finalise style, add reset function, add counting function with game finish condition. Add interactivity to control icons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,140 @@
 //Declare variables:
 const reply = ['rock', 'paper','scissors','well'];
 let currentReply;
+let count = 0;
+let score = 0;
 
 //Setting up event listeners
 document.getElementById('rock').addEventListener('click', myRock);
 document.getElementById('paper').addEventListener('click', myPaper);
 document.getElementById('scissors').addEventListener('click', myScissors);
 document.getElementById('well').addEventListener('click', myWell);
+document.getElementById('rock').addEventListener('click', myCount);
+document.getElementById('paper').addEventListener('click', myCount);
+document.getElementById('scissors').addEventListener('click', myCount);
+document.getElementById('well').addEventListener('click', myCount);
+document.getElementById('repeat').addEventListener('click', finishedReload);
 
 //Declare functions
 function myRock() {
-    currentReply = reply[(Math.floor(Math.random() * reply.length))];;
-    document.getElementById('reply1').innerHTML = currentReply;
+    currentReply = reply[(Math.floor(Math.random() * reply.length))];
+    const node = document.createElement('h2');    
     if (currentReply === 'rock') {
-        document.getElementById('result1').innerHTML = 'DRAW';
+        const textnode = document.createTextNode('Replied ROCK. DRAW');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 0;        
     } else if (currentReply === 'scissors') {
-        document.getElementById('result1').innerHTML = 'WON!';
+        const textnode = document.createTextNode('Replied: SCISSORS. You WON!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 1;        
+    } else if (currentReply === 'paper') {
+        const textnode = document.createTextNode('Replied: PAPER. You LOST!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score - 1;
     } else {
-        document.getElementById('result1').innerHTML = 'lost';
+        const textnode = document.createTextNode('Replied: WELL. You LOST!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score - 1;        
     }
+    document.getElementById('rock').remove();
 }
 
 function myPaper() {
-    currentReply = reply[(Math.floor(Math.random() * reply.length))];;
-    document.getElementById('reply2').innerHTML = currentReply;
+    currentReply = reply[(Math.floor(Math.random() * reply.length))];
+    const node = document.createElement('h2');    
     if (currentReply === 'paper') {
-        document.getElementById('result2').innerHTML = 'DRAW';
-    } else if (currentReply === 'rock' || currentReply === 'well') {
-        document.getElementById('result2').innerHTML = 'WON!';
+        const textnode = document.createTextNode('Replied PAPER. DRAW');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 0;
+
+    } else if (currentReply === 'scissors') {
+        const textnode = document.createTextNode('Replied: SCISSORS. You LOST!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score - 1;
+    } else if (currentReply === 'rock') {
+        const textnode = document.createTextNode('Replied: ROCK. You WON!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 1;
     } else {
-        document.getElementById('result2').innerHTML = 'lost';
+        const textnode = document.createTextNode('Replied: WELL. You WON!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 1;
     }
+    document.getElementById('paper').remove();    
 }
 
 function myScissors() {
-    currentReply = reply[(Math.floor(Math.random() * reply.length))];;
-    document.getElementById('reply3').innerHTML = currentReply;
+    currentReply = reply[(Math.floor(Math.random() * reply.length))];
+    const node = document.createElement('h2');    
     if (currentReply === 'scissors') {
-        document.getElementById('result3').innerHTML = 'DRAW';
+        const textnode = document.createTextNode('Replied SCISSORS. DRAW');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 0;
+    } else if (currentReply === 'rock') {
+        const textnode = document.createTextNode('Replied: ROCK. You LOST!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score - 1;
     } else if (currentReply === 'paper') {
-        document.getElementById('result3').innerHTML = 'WON!';
+        const textnode = document.createTextNode('Replied: PAPER. You WON!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 1;
     } else {
-        document.getElementById('result3').innerHTML = 'lost';
+        const textnode = document.createTextNode('Replied: WELL. You LOST!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score - 1;
+    }
+    document.getElementById('scissors').remove();    
+}
+
+function myWell() {
+    currentReply = reply[(Math.floor(Math.random() * reply.length))];
+    const node = document.createElement('h2');    
+    if (currentReply === 'well') {
+        const textnode = document.createTextNode('Replied WELL. DRAW');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 0;
+    } else if (currentReply === 'rock') {
+        const textnode = document.createTextNode('Replied: ROCK. You WON!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 1;
+    } else if (currentReply === 'paper') {
+        const textnode = document.createTextNode('Replied: PAPER. You LOST!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score - 1;
+    } else {
+        const textnode = document.createTextNode('Replied: SCISSORS. You WON!');
+        node.appendChild(textnode);
+        document.getElementById('result').appendChild(node);
+        score = score + 1;
+    }
+    document.getElementById('well').remove();    
+}
+function myCount() {
+    count++;    
+    if (count === 3) {
+        document.getElementById('total').innerHTML = score;
+        console.log('finish');
+        console.log(score);
+    } else if (count > 3) {        
+        location.reload();
     }
 }
-//Under construction:
-function myWell() {
-    currentReply = reply[(Math.floor(Math.random() * reply.length))];;
-    /*
-    document.getElementById('reply3').innerHTML = currentReply;
-    if (currentReply === 'scissors') {
-        document.getElementById('result3').innerHTML = 'DRAW';
-    } else if (currentReply === 'paper') {
-        document.getElementById('result3').innerHTML = 'WON!';
-    } else {
-        document.getElementById('result3').innerHTML = 'lost';
-    }*/
+
+function finishedReload () {
+    location.reload();
 }
